def0ef36f65180a27ebdf9abaf2daaf5cea67a71
Amir Hossain <ah2324@en-ec-slurm-instructional-login.coecis.cornell.edu>
2023-12-06 11:39:14 -0500

    adding files

--- lab5_fast/kernel/matrix_ops.c
+++ lab5_fast/kernel/matrix_ops.c
@@ -1,4 +1,6 @@
 #include "matrix_ops.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 float **matmul(float **A, float **B, int A_rows, int A_cols, int B_rows, int B_cols) {
     if (A_cols != B_rows) {
@@ -6,18 +8,23 @@ float **matmul(float **A, float **B, int A_rows, int A_cols, int B_rows, int B_c
         return NULL;
     }
 
+    // Allocate memory for the result matrix
     float **C = (float **)malloc(A_rows * sizeof(float *));
     for (int i = 0; i < A_rows; i++) {
         C[i] = (float *)malloc(B_cols * sizeof(float));
-    }
-
-    for (int i = 0; i < A_rows; i++) {
         for (int j = 0; j < B_cols; j++) {
             C[i][j] = 0.0;
-            for (int k = 0; k < A_cols; k++) {
+        }
+    }
+
+    // Loop order changed to optimize cache usage
+    for (int i = 0; i < A_rows; ++i) {
+        for (int k = 0; k < A_cols; ++k) {
+            for (int j = 0; j < B_cols; ++j) {
                 C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
+
     return C;
 }
--- lab5_fast/kernel/nn.c
+++ lab5_fast/kernel/nn.c
@@ -1,84 +1,92 @@
 #include "nn.h"
+#include <stdlib.h>
+
+#define CONV_OUTPUT_CHANNELS 32 // Replace 32 with the actual constant if different
 
 float *flatten(float ***input, int inputSize, int depth)
 {
-  if (inputSize <= 0 || depth <= 0)
-  {
-    return NULL;
-  }
-
-  int outputSize = inputSize * inputSize * depth;
-  float *output = malloc(outputSize * sizeof(*output));
-  int idx = 0;
-
-  for (int k = 0; k < depth; k++)
-  {
-    for (int i = 0; i < inputSize; i++)
+    if (inputSize <= 0 || depth <= 0)
     {
-      for (int j = 0; j < inputSize; j++)
-      {
-        output[idx++] = input[k][i][j];
-      }
+        return NULL;
     }
-  }
 
-  return output;
+    int outputSize = inputSize * inputSize * depth;
+    float *output = (float *)malloc(outputSize * sizeof(float));
+    if (!output) return NULL; // Check for malloc failure
+
+    float *outPtr = output;
+    for (int k = 0; k < depth; k++)
+    {
+        for (int i = 0; i < inputSize; i++)
+        {
+            for (int j = 0; j < inputSize; j++)
+            {
+                *outPtr++ = input[k][i][j];
+            }
+        }
+    }
+
+    return output;
 }
 
 void destroyConvOutput(float ***convOutput, int convOutputSize)
 {
-  for (int i = 0; i < 32; i++)
-  {
-    for (int j = 0; j < convOutputSize; j++)
+    for (int i = 0; i < CONV_OUTPUT_CHANNELS; i++)
     {
-      free(convOutput[i][j]);
+        for (int j = 0; j < convOutputSize; j++)
+        {
+            free(convOutput[i][j]);
+        }
+        free(convOutput[i]);
     }
-    free(convOutput[i]);
-  }
-  free(convOutput);
+    free(convOutput);
 }
 
 int forwardPass(float ***image, int numChannels, float ****conv1WeightsData, float **fc1WeightsData, float **fc2WeightsData, float *conv1BiasData, float *fc1BiasData, float *fc2BiasData)
 {
-  // 1. Perform the convolution operation
-  float ***convOutput = convolution_im2col(image, numChannels, conv1WeightsData, conv1BiasData, 32, 28, 3);
+    // 1. Perform the convolution operation
+    float ***convOutput = convolution_im2col(image, numChannels, conv1WeightsData, conv1BiasData, CONV_OUTPUT_CHANNELS, 28, 3);
 
-  // 2. Flatten the output
-  float *fcInput = flatten(convOutput, 26, 32);
+    // 2. Flatten the output
+    float *fcInput = flatten(convOutput, 26, CONV_OUTPUT_CHANNELS);
+    if (!fcInput) {
+        destroyConvOutput(convOutput, 26); // Free memory in case of failure
+        return -1; // Indicate failure
+    }
 
-  // 3. Perform the fully connected operations
-  float *fcOutput1 = linear(fcInput, fc1WeightsData, fc1BiasData, 26 * 26 * 32, 128);
-  applyRelu(fcOutput1, 128);
-  float *fcOutput2 = linear(fcOutput1, fc2WeightsData, fc2BiasData, 128, 10);
+    // 3. Perform the fully connected operations
+    float *fcOutput1 = linear(fcInput, fc1WeightsData, fc1BiasData, 26 * 26 * CONV_OUTPUT_CHANNELS, 128);
+    applyRelu(fcOutput1, 128);
+    float *fcOutput2 = linear(fcOutput1, fc2WeightsData, fc2BiasData, 128, 10);
 
-  // 4. Apply the final softmax activation
-  float *softmaxOutput = softmax(fcOutput2, 10);
+    // 4. Apply the final softmax activation
+    float *softmaxOutput = softmax(fcOutput2, 10);
 
-  // 5. Make predictions
-  int predictedClass = predict(softmaxOutput, 10);
+    // 5. Make predictions
+    int predictedClass = predict(softmaxOutput, 10);
 
-  // Clean up the memory usage
-  destroyConvOutput(convOutput, 26);
-  free(fcInput);
-  free(fcOutput1);
-  free(fcOutput2);
-  free(softmaxOutput);
+    // Clean up the memory usage
+    destroyConvOutput(convOutput, 26);
+    free(fcInput);
+    free(fcOutput1);
+    free(fcOutput2);
+    free(softmaxOutput);
 
-  return predictedClass;
+    return predictedClass;
 }
 
-int predict(float *probabilityVector, int numClasses)
+inline int predict(float *probabilityVector, int numClasses) // inlined for potential performance improvement
 {
-  int predictedClass = 0;
-  float maxProbability = probabilityVector[0];
+    int predictedClass = 0;
+    float maxProbability = probabilityVector[0];
 
-  for (int i = 1; i < numClasses; i++)
-  {
-    if (probabilityVector[i] > maxProbability)
+    for (int i = 1; i < numClasses; i++)
     {
-      maxProbability = probabilityVector[i];
-      predictedClass = i;
+        if (probabilityVector[i] > maxProbability)
+        {
+            maxProbability = probabilityVector[i];
+            predictedClass = i;
+        }
     }
-  }
-  return predictedClass;
+    return predictedClass;
 }
